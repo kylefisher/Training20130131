777c9b733e5e34d5ae925489fc0f282b31a99490
Kyle Fisher <kyle@kylefisher.org>
2013-01-31 15:05:05 -0600

    abcdefg

---
+++ app.js
@@ -0,0 +1,27 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: KFisher
+ * Date: 1/31/13
+ * Time: 2:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var app = require('http').createServer(handler)
+    , fs = require('fs')
+
+app.listen(3000);
+
+function handler (req, res) {
+    fs.readFile('jquery.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading jquery.htm');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+console.log("Server listening on port 3000");
